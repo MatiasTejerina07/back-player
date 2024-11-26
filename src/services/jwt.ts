a070a9be6ff7727fb7c60c67f1484132685c371c
Mat <matiastejerina94@gmail.com>
2024-11-26 12:43:26 -0300

    fix to secret

--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -2,10 +2,17 @@ import { IJWT } from "./interfaces/IJWT";
 import jwt from "jsonwebtoken";
 
 export class JWT implements IJWT {
+  private secret: string;
+  constructor() {
+    if (!process.env.SECRET) {
+      throw new Error("Ha ocurrido un error COD74A");
+    }
+    this.secret = process.env.SECRET;
+  }
   async signin(payload: string): Promise<string> {
-    return jwt.sign({ data: payload }, "secret", { expiresIn: "1h" });
+    return jwt.sign({ data: payload }, this.secret, { expiresIn: "1h" });
   }
   verify(token: string): string {
-    return jwt.verify(token, "secret") as string;
+    return jwt.verify(token, this.secret) as string;
   }
 }
